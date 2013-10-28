fab890bca554716a9ad1d087d749e36333532737
ajing <ajingnk@gmail.com>
2013-10-28 14:28:37 -0400

    add more random loops for each parameter

--- Research/SeqStat.py
+++ Research/SeqStat.py
@@ -23,31 +23,53 @@ def SeqRandom(n):
 def MakeFilename(N,n):
     return "N_{0}_n_{1}.jpg".format(N,n)
 
-def DrawFigure(xnames, yvalue, filename):
+def DrawFigure(xnames, yvalue, errorbar, filename):
     ylen = len(yvalue)
-    mpl.rcParams['xtick.major.pad'] = 8
     fig, ax = plt.subplots()
-    ax.bar(np.arange(ylen),yvalue)
+    width   = 1
+    ax.bar(np.arange(ylen), yvalue, width, yerr=errorbar)
     num_locator = ylen / 20 if ylen/20 > 0 else 1
-    myLocator = mticker.MultipleLocator(num_locator)
-    ax.xaxis.set_major_locator(myLocator)
-    plt.xticks(np.arange(ylen) + 0.5, xnames)
-    plt.setp(ax.get_xticklabels(), fontsize=10, rotation='vertical')
+    if num_locator < 2:
+        plt.xticks(np.arange(ylen) + 0.5, xnames)
+    else:
+        plt.xticks(np.arange(ylen) + 0.5, xnames)
+        myLocator = mticker.MultipleLocator(num_locator)
+        ax.xaxis.set_major_locator(myLocator)
+    plt.axis('tight')
+    plt.setp(ax.get_xticklabels(), rotation='vertical')
     plt.savefig(filename, format = "jpg")
     plt.close()
 
+def SortOrder(s):
+    return sorted(range(len(s)), key=lambda k: s[k])
+
 def SeqStat():
     # total length of sequence
-    N = 1000000
-    rand_seq = SeqRandom(N)
-    for N in 10**np.arange(1,9):
-        for n in range(2,5):
+    N = 500
+    # number of random times
+    rand_num = 1
+    for n in range(2, 5):
+        # n is the size of pattern
+        percent_matrix = np.zeros( (rand_num, 4**n) )
+        for t in range(rand_num):
+            rand_seq = SeqRandom(N)
             kmerdict = KmerFuzzy(rand_seq, n, 0)
             patterns = kmerdict.keys()
             pattern_frequency = np.array(kmerdict.values())
             pattern_percent   = pattern_frequency * 1.0 / sum(pattern_frequency)
-            figname = MakeFilename(N,n)
-            DrawFigure(patterns, pattern_percent, figname)
+            percent_matrix[t,] = pattern_percent
+        # average percent for each pattern
+        print percent_matrix.size
+        aver_percent = np.mean( percent_matrix, axis = 0)
+        print len(aver_percent)
+        std_percent = np.std( percent_matrix, axis = 0)
+        print len(std_percent)
+        percent_order     = SortOrder(aver_percent)
+        patterns_reorder  = [ patterns[i] for i in percent_order ]
+        percent_reorder = [ aver_percent[i] for i in percent_order ]
+        std_reorder     = [ std_percent[i] for i in percent_order ]
+        figname = MakeFilename(N,n)
+        DrawFigure(patterns_reorder, percent_reorder, std_reorder, figname)
 
 if __name__ == "__main__":
     SeqStat()
