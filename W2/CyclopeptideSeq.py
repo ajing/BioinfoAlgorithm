8d7c4eef56a39687c44e4f6d98fe4cf4a4788ec7
ajing <ajingnk@gmail.com>
2013-11-09 10:40:02 -0500

    working version of cyclopeptide

--- W2/CyclopeptideSeq.py
+++ W2/CyclopeptideSeq.py
@@ -31,8 +31,30 @@ def CheckDuplicate(alist):
             print "Yes duplicates!"
     print "no duplicates"
 
+def SubPeptideLinear( peptide, length  ):
+    pep_len = len(peptide)
+    sublist = []
+    for i in range(pep_len - length):
+        sublist.append(peptide[i:i + length])
+    return sublist
+
+def AllPossibleSubPeptideLinear( peptide ):
+    pep_len = len(peptide)
+    all_possible = [""]
+    for i in range(1,pep_len):
+        all_possible += SubPeptideLinear(peptide, i)
+    all_possible.append(peptide)
+    return all_possible
+
+def Linearspectrum(aacid_mass_list):
+    mass_combine = AllPossibleSubPeptideLinear(aacid_mass_list)
+    mass_combine = [x for x in mass_combine]
+    mass_sum     = [sum(x) for x in mass_combine]
+    mass_sum     = list(set(mass_sum))
+    return sorted(mass_sum)
+
 def Consistent(masslist, spectrum):
-    mass_spectrum = Cyclospectrum(masslist)
+    mass_spectrum = Linearspectrum(masslist)
     for each in mass_spectrum:
         if not each in spectrum:
             return False
@@ -49,12 +71,13 @@ def Cyclospectrum(aacid_mass_list):
     return sorted(mass_sum)
 
 def CyclopeptideSequencing(spectrum_list):
+    # clean spectrum_list
+    spectrum_list = sorted(list(set(spectrum_list)))
     candidates = [0]
     outputlist = []
     while candidates:
-        print len(candidates)
+    #for i in range(2):
         candidates = Expand(candidates)
-        print len(candidates)
         candidates_new = []
         for peptide in candidates:
             if Cyclospectrum(peptide) == spectrum_list:
@@ -62,15 +85,16 @@ def CyclopeptideSequencing(spectrum_list):
             elif Consistent(peptide, spectrum_list):
                 candidates_new.append(peptide)
         candidates = candidates_new
-        print candidates
     return outputlist
 
 if __name__ == "__main__":
     massfile = "/home/ajing/Downloads/dataset_22_4.txt"
-    masslist = open(massfile).readlines()[0].strip().split()
+    mass_list = open(massfile).readlines()[0].strip().split()
     #masslist = [0,113,128,186,241,299,314,427]
-    masslist = "0 71 97 99 103 113 113 114 115 131 137 196 200 202 208 214 226 227 228 240 245 299 311 311 316 327 337 339 340 341 358 408 414 424 429 436 440 442 453 455 471 507 527 537 539 542 551 554 556 566 586 622 638 640 651 653 657 664 669 679 685 735 752 753 754 756 766 777 782 782 794 848 853 865 866 867 879 885 891 893 897 956 962 978 979 980 980 990 994 996 1022 1093".split()
-    masslist = map( int, masslist )
-    print masslist
-    peptidelist = CyclopeptideSequencing(sorted(masslist))
+    #mass_list = "0 71 97 99 103 113 113 114 115 131 137 196 200 202 208 214 226 227 228 240 245 299 311 311 316 327 337 339 340 341 358 408 414 424 429 436 440 442 453 455 471 507 527 537 539 542 551 554 556 566 586 622 638 640 651 653 657 664 669 679 685 735 752 753 754 756 766 777 782 782 794 848 853 865 866 867 879 885 891 893 897 956 962 978 979 980 980 990 994 996 1022 1093".split()
+    mass_list = map( int, mass_list )
+    peptidelist = CyclopeptideSequencing(sorted(mass_list))
+    print "length:", len(peptidelist)
     print " ".join([ "-".join(map(str, peptide)) for peptide in peptidelist])
+    mass_list = "113-113-115-99".split("-")
+    mass_list = map( int, mass_list )
