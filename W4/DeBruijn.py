17d64f4a7def452ef20b2e97245378111682fed7
ajing <ajingnk@gmail.com>
2013-12-02 14:11:25 -0500

    eulerian cycle

--- W4/DeBruijn.py
+++ W4/DeBruijn.py
@@ -6,12 +6,16 @@ from StringComp import StringComp
 from OverlapGraph import GraphOverlap
 from collections import defaultdict
 
+def SimpList(adjlist):
+    collapse_dict = defaultdict(list)
+    for start, end in adjlist:
+        collapse_dict[start].append(end)
+    return collapse_dict
+
 def DeBruijnk(seq, N):
     motif_list    = StringComp(seq, N - 1)
     adj_list      = zip(motif_list[:-1], motif_list[1:])
-    collapse_dict = defaultdict(list)
-    for start, end in adj_list:
-        collapse_dict[start].append(end)
+    collapse_dict = SimpList(adj_list)
     return collapse_dict
 
 def PrintDict(col_dict):
@@ -22,10 +26,24 @@ def PrintDict(col_dict):
         end = list(set(end))
         print " -> ".join([start, ",".join(sorted(end))])
 
-if __name__ == "__main__":
+def DeBruijn_main():
     #PrintDict(DeBruijnk("AAGATTCTCTAC", 4))
     infile  = "/home/ajing/Downloads/dataset_53_6.txt"
     content = [line.strip() for line in open(infile).readlines()]
     N       = int(content[0])
     Seq     = content[1]
     PrintDict(DeBruijnk(Seq, N))
+
+def DeBruijnKmer(seqs):
+    adj_list = []
+    for eachseq in seqs:
+        adj_list.append([eachseq[:-1], eachseq[1:]])
+    collapse_dict = SimpList(adj_list)
+    return collapse_dict
+
+if __name__ == "__main__":
+    #DeBruijn_main()
+    infile  = "/home/ajing/Downloads/dataset_54_7.txt"
+    content = [line.strip() for line in open(infile).readlines()]
+    seqs = content
+    PrintDict(DeBruijnKmer(seqs))
---
+++ W4/EulerianCyc.py
@@ -0,0 +1,75 @@
+'''
+    To achieve this runtime speedup, you would need to use an efficient data structure in order to maintain the current cycle that Leo is building as well the list of unused edges incident to each node and the list of nodes on the current cycle that have unused edges.
+     Input: The adjacency list of an Eulerian directed graph.
+     Output: An Eulerian cycle in this graph.
+'''
+
+from collections import defaultdict
+from random import choice
+
+def RandomWalk(graph_dict, start):
+    current = start
+    cycle   = [current]
+    while True:
+        next_points = graph_dict[current]
+        next_point = choice(next_points)
+        graph_dict[current].remove(next_point)
+        if next_point == start:
+            break
+        current    = next_point
+        cycle.append(current)
+    return cycle
+
+def ParseGraph(infile):
+    content = [line.strip() for line in open(infile).readlines()]
+    graph_dict = dict()
+    for line in content:
+        start, end = line.split(" -> ")
+        graph_dict[start] = end.split(",")
+    return graph_dict
+
+def EdgesFromGraph(graph_dict):
+    edges = []
+    for key, value in graph_dict.items():
+        if not value:
+            continue
+        for eachvalue in value:
+            edges.append([key, eachvalue])
+    return edges
+
+def AvailNodes(cycle, unused_edges):
+    avail_nodes = []
+    for edge in unused_edges:
+        if edge[0] in cycle and not edge[0] in avail_nodes:
+            avail_nodes.append(edge[0])
+        if edge[1] in cycle and not edge[1] in avail_nodes:
+            avail_nodes.append(edge[1])
+    return avail_nodes
+
+def RearrangeCycle(cycle, index):
+    return cycle[index:] + cycle[:index]
+
+
+def EulerianCyc(de_graph):
+    # debruijn graph is a dictionary
+    rand_start    = choice(de_graph.keys())
+    current_cycle = RandomWalk(de_graph, rand_start)
+    unused_edges  = EdgesFromGraph(de_graph)
+    while unused_edges:
+        unexplore_node = choice(AvailNodes(current_cycle, unused_edges))
+        unexplore_index = current_cycle.index(unexplore_node)
+        rearrange_old  = RearrangeCycle(current_cycle, unexplore_index)
+        add_cycle = RandomWalk(de_graph, unexplore_node)
+        current_cycle = rearrange_old + add_cycle
+        unused_edges  = EdgesFromGraph(de_graph)
+    return current_cycle
+
+if __name__ == "__main__":
+    infile  = "/home/ajing/Downloads/dataset_57_2.txt"
+    graph   = ParseGraph(infile)
+    cycle   = EulerianCyc(graph)
+    index   = cycle.index("1140")
+    cycle   = RearrangeCycle(cycle, index)
+    cycle.append(cycle[0])
+    #print cycle
+    print "->".join(cycle)
