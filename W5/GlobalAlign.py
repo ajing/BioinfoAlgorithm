fd640842d04489e12f00818ee951c6e21b0f1291
ajing <ajingnk@gmail.com>
2013-12-17 14:13:39 -0500

    workingn version for global alignment

--- W5/GlobalAlign.py
+++ W5/GlobalAlign.py
@@ -3,9 +3,10 @@
    Input: Two protein strings written in the single-letter amino acid alphabet.
    Output: The maximum alignment score of these strings followed by an alignment achieving this maximum score. Use the BLOSUM62 scoring matrix and indel penalty 5
 '''
-from collections import defaultdict
+import numpy as np
 
 gap_score = 5
+BLOSUM = "BLOSUM62.txt"
 
 def ReturnScore(amino1, amino2, score):
     if amino1 == "-" or amino2 == "-":
@@ -14,7 +15,7 @@ def ReturnScore(amino1, amino2, score):
         return score[amino1][amino2]
 
 def ParseMatrix(infile):
-    score = defaultdict(lambda: defaultdict(int))
+    dmatrix = dict()
     flag  = 1  #first line flag
     i     = 0
     for line in open(infile):
@@ -25,16 +26,60 @@ def ParseMatrix(infile):
             continue
         rowname = colnames[i]
         for j in range(len(colnames)):
-            score[rowname][colnames[j]] = int(content[j+1])
+            dmatrix[rowname, colnames[j]] = int(content[j+1])
         i += 1
-    return score
+    return dmatrix
 
-def MaxScore(v, w, i, j, score, backtrack):
+def MaxScore(v, w, i, j, score, dmatrix, backtrack):
     maxscore = -1000
-    # v is empty
-    newscore = score[i - 1, j] + ReturnScore(v[i - 1], "-")
+    # v is gap
+    newscore = score[i - 1, j]  - gap_score
     if newscore > maxscore:
-        backtrack[i, j] =
+        backtrack[i, j] = 1
+        maxscore = newscore
+    # w is gap
+    newscore = score[i, j - 1]  - gap_score
+    if newscore > maxscore:
+        backtrack[i, j] = 2
+        maxscore = newscore
+    newscore = score[i - 1, j - 1] + dmatrix[v[i - 1], w[j - 1]]
+    if newscore > maxscore:
+        backtrack[i, j] = 3
+        maxscore = newscore
+    return maxscore
+
+
+def ScoreInitialize(score):
+    # add penalty score for each side
+    rownum, colnum = score.shape
+    for i in range(rownum):
+        score[i, 0] = - i * gap_score
+    for j in range(colnum):
+        score[0, j] = - j * gap_score
+
+def PrintBacktrack(backtrack, v, w):
+    i = len(v)
+    j = len(w)
+    matchlist = []
+    while i > 0 or j > 0:
+        if backtrack[i, j] == 3:
+            matchlist.append([v[i - 1], w[j - 1]])
+            i -= 1
+            j -= 1
+        elif backtrack[i, j] == 2:
+            matchlist.append(["-", w[j - 1]])
+            j -= 1
+        else:
+            matchlist.append([v[i - 1], "-"])
+            i -= 1
+    matchlist.reverse()
+    firstline = []
+    secondline = []
+    for i in range(len(matchlist)):
+        firstline.append(matchlist[i][0])
+        secondline.append(matchlist[i][1])
+    print "".join(firstline)
+    print "".join(secondline)
 
 
 def GlobalAlign(v, w):
@@ -42,11 +87,23 @@ def GlobalAlign(v, w):
     v_len = len(v)
     w_len = len(w)
     score = np.zeros([v_len + 1, w_len + 1])
+    ScoreInitialize(score)
+    # 1 is | (down); 2 is -> left; 3 is \ diag
+    backtrack = np.zeros([v_len + 1, w_len + 1])
+    dmatrix = ParseMatrix(BLOSUM)
     for i in range(1, 1 + v_len):
         for j in range(1, 1 + w_len):
-            if v[i - 1] == w[j - 1]:
-
+            score[i, j] = MaxScore(v, w, i, j, score, dmatrix, backtrack)
+    print int(score[v_len, w_len])
+    PrintBacktrack(backtrack, v, w)
 
 if __name__ == "__main__":
-    blosum = "BLOSUM62.txt"
-    ParseMatrix(blosum)
+    v = "PLEASANTLY"
+    w = "MEANLY"
+    #v = "TNIYGLKERVPR"
+    #w = "TNNMQTRMWCVLIAAPCHLW"
+    infile  = "/home/ajing/Downloads/dataset_76_3.txt"
+    v, w = [ x.strip() for x in open(infile).readlines() ]
+    #print v
+    #print w
+    GlobalAlign(v, w)
