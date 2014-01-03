798aff049997156dd796c844fdabd54cbf3cb121
ajing <ajingnk@gmail.com>
2014-01-02 22:06:20 -0500

    overlap of two sequence

---
+++ W5/Overlap.py
@@ -0,0 +1,5 @@
+'''
+Solve the Overlap Alignment Problem.
+    Input: Two strings v and w, each of length at most 1000.
+    Output: The score of an optimal overlap alignment of v and w, followed by an alignment of a suffix v of v and a prefix of w achieving this maximum score. Use an alignment score in which matches count +1 and both the mismatch and indel penalties are 2.
+'''
