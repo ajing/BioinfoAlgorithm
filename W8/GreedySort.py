42e9276e858511eecba1f64dd62a8285069117a0
ajing <ajingnk@gmail.com>
2014-01-07 17:59:49 -0500

    initial commit for greedy sort

---
+++ W8/GreedySort.py
@@ -0,0 +1,6 @@
+'''
+Implement GREEDYSORTING.
+     Input: A permutation P.
+          Output: The sequence of permutations corresponding to applying GREEDYSORTING to P, ending with
+               the identity permutation.
+'''
