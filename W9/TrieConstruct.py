5d3480daaca4a247f280717013bcc535b0f254c7
ajing <ajingnk@gmail.com>
2014-01-12 23:48:25 -0500

    just a few comments

---
+++ W9/TrieConstruct.py
@@ -0,0 +1,9 @@
+'''
+Solve the Trie Construction Problem.
+     Input: A collection of strings Patterns.
+     Output: The adjacency list corresponding to Trie(Patterns), in the following format. If Trie(Patterns) has
+     n nodes, first label the root with 1 and then label the remaining nodes with the integers 2 through n in
+     any order you like. Each edge of the adjacency list of Trie(Patterns) will be encoded by a triple: the first
+     two members of the triple must be the integers labeling the initial and terminal nodes of the edge,
+     respectively; the third member of the triple must be the symbol labeling the edge.
+'''
