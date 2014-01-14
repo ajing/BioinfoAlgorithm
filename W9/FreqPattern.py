c05cd820928a0c0d3fce2654aec6564c46051b2d
ajing <ajingnk@gmail.com>
2014-01-13 21:14:26 -0500

    a simple version to find frequence patterns

---
+++ W9/FreqPattern.py
@@ -0,0 +1,64 @@
+'''
+'''
+
+from SuffixTree import BuildSuffixTree, MatchEdge, IndexSame
+
+def PatternInTree(pattern, stree):
+    startnode = 1
+    while pattern:
+        matched_s = MatchEdge(pattern, stree, startnode)
+        if matched_s:
+            index   = IndexSame(matched_s, pattern)
+            pattern = pattern[index:]
+            startnode, index = stree[startnode][matched_s]
+        else:
+            return False
+    return True
+
+def PatternGenerator(text, n):
+    for i in range(len(text) - n + 1):
+        yield text[i:n + i]
+
+def FreqPatternExist(text, n):
+    patterns = [ text[i:n + i] for i in range(len(text) - n + 1) ]
+    for pattern in patterns:
+        if patterns.count(pattern) > 1:
+            return pattern
+    return False
+
+def FreqPattern(text):
+    suffixtree = BuildSuffixTree(text)
+    print suffixtree
+    startlen   = len(text) * 2/5
+    for i in range(startlen, len(text)):
+        for pattern in PatternGenerator(text[:-1], i):
+            print pattern
+            if PatternInTree(pattern, suffixtree):
+                bestp = pattern
+    return bestp
+
+def FreqCheat(text):
+    bestp = ""
+    startlen   = len(text) * 2/5
+    for i in range(startlen, len(text)):
+        pattern = FreqPatternExist(text, i)
+        if pattern:
+            bestp = pattern
+    return bestp
+
+def test():
+    print list(PatternGenerator("ABCD", 1))
+    print list(PatternGenerator("ABCD", 2))
+    stree = BuildSuffixTree("ABCDD$")
+    assert(PatternInTree("ABC", stree))
+    assert(PatternInTree("CA", stree) == False)
+    assert(PatternInTree("ABD", stree) == False)
+    print FreqPattern("ABCDBCD$")
+    print FreqCheat("ABCDBCD$")
+    print FreqCheat("AAA$")
+
+if __name__ == "__main__":
+    #test()
+    infile   = "/home/ajing/Downloads/dataset_93_3.txt"
+    text = open(infile).readlines()[0].strip()
+    print FreqCheat(text)
--- W9/SuffixTree.py
+++ W9/SuffixTree.py
@@ -6,24 +6,25 @@ Solve the Suffix Tree Construction Problem.
 
 def IndexSame(string1, string2):
     min_s = min(len(string1), len(string2))
+    if string1.startswith(string2) or string2.startswith(string1):
+        return min_s
     for i in range(min_s):
         if string1[i] != string2[i]:
             return i
 
 def MatchEdge( eachc, suffixtree, startnode):
-    max_len = 0
+    max_ind = 0
     max_key = False
     for eachkey in suffixtree[startnode]:
         index = IndexSame(eachkey, eachc)
-        if  index > max_len:
+        if  index > max_ind:
             max_key = eachkey
-            max_len = index
+            max_ind = index
     return max_key
 
 def CurrentID():
     CurrentID.ID += 1
     return CurrentID.ID
-CurrentID.ID = 1
 
 def FindCommonPrefix(pattern, stree):
     startnode = 1
@@ -41,8 +42,11 @@ def FindCommonPrefix(pattern, stree):
             startold = startnode
             startnode, index = stree[startnode][matched_s]
         else:
-            print "pattern_old:", pattern_old, startold, matched_old
-            return pattern_old, startold, matched_old
+            #print "pattern_old:", pattern_old, startold, matched_old
+            if matched_old in stree[startnode]:
+                return pattern, startnode, matched_s
+            else:
+                return pattern_old, startold, matched_old
 
 def ExtendSuffixTree(pattern_left, ematched, stree, matchnode, i):
     branch = stree[matchnode]
@@ -53,10 +57,12 @@ def ExtendSuffixTree(pattern_left, ematched, stree, matchnode, i):
         stree.append({})
         new_w = CurrentID()
         stree.append({"!":i})
+        #print "branch", branch
         branch[ematched[:index]] = [ new_v, i ]
         stree[new_v][ematched[index:]] = branch[ematched]
         stree[new_v][pattern_left[index:]] = [ new_w, i]
         del branch[ematched]
+        #print "branch after", branch
     else:
         new_v = CurrentID()
         stree.append({"!":i})
@@ -70,22 +76,29 @@ def PrintSuffix(suffixtree):
     print "\n".join(edge_list)
 
 def BuildSuffixTree(text):
+    # initialize the first id
+    CurrentID.ID = 1
     # suffix tree is like [0, {"CTG":(nextnode, startindex), "A"}, {}]
     patterns = [text[i:] for i in range(len(text))]
     suffixtree = [{},{}]
     for i in range(len(patterns)):
         eachp  = patterns[i]
-        print eachp
+        #print "eachp:", eachp
         patternleft, nodematched, edgematched = FindCommonPrefix(eachp, suffixtree)
         ExtendSuffixTree( patternleft, edgematched, suffixtree, nodematched, i)
+        #print "suffixtree:", suffixtree
     return suffixtree
 
 def test():
     suftree = BuildSuffixTree("ATAAA$")
     print suftree
     PrintSuffix(suftree)
+    #print IndexSame("A", "AA")
 
 
 if __name__ == "__main__":
-    test()
-
+    #test()
+    infile   = "/home/ajing/Downloads/dataset_93_3.txt"
+    text = open(infile).readlines()[0].strip()
+    stree = BuildSuffixTree(text)
+    PrintSuffix(stree)
