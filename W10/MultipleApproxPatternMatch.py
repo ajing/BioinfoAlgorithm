bae8d3482fd07f4c08577d1a9bbecc6199d02df6
ajing <ajingnk@gmail.com>
2014-01-21 15:10:57 -0500

    brute force solution for multiple dismatch problem

--- W10/MultipleApproxPatternMatch.py
+++ W10/MultipleApproxPatternMatch.py
@@ -5,30 +5,55 @@ Solve the Multiple Approximate Pattern Matching Problem.
 '''
 
 import regex
+from multiprocessing import Pool
+import math
 
 def GetRE(pattern, d):
     reg = r'(?:' + pattern + "){s<=" + str(d) + "}"
     return reg
 
-def MultipleApproxPatternMatch(text, patterns, d):
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+def MultipleApproxPatternMatch(inputset):
+    patterns, d = inputset
     all_matched_index = []
     for eachp in patterns:
         reg = GetRE(eachp, d)
         for m in regex.finditer(reg, text, overlapped = True):
             all_matched_index.append(m.start())
-    all_matched_index.sort()
-    print " ".join(map(str, all_matched_index))
+    return all_matched_index
 
 def test():
     text = "ACATGCTACTTT"
     patterns = [ "ATT", "GCC", "GCTA", "TATT"]
     d = 1
-    MultipleApproxPatternMatch(text, patterns, d)
+    #MultipleApproxPatternMatch(text, patterns, d)
+    Multiprocessor(text, patterns, d, 2)
+
+def Multiprocessor(text, patterns, d, k):
+    # k is the number of processors
+    part_len      = math.ceil(len(patterns) / k)
+    patterns_list = list(chunks(patterns, k))
+    argumentlist  = []
+    for eachp in patterns_list:
+        argumentlist.append((eachp, d))
+    MultipleApproxPatternMatch(argumentlist[0])
+    pool = Pool(processes = k)
+    result = pool.map_async(MultipleApproxPatternMatch, argumentlist)
+    matched_list    = result.get()
+    matched_flatten = [item for sublist in matched_list for item in sublist]
+    matched_flatten.sort()
+    print " ".join(map(str, matched_flatten))
 
 if __name__ == "__main__":
     '''
     test()
     '''
+    global text
     infile   = "tmp"
     infile   = "/home/ajing/Downloads/dataset_104_6.txt"
     content = [ x.strip() for x in open(infile).readlines()]
@@ -36,4 +61,5 @@ if __name__ == "__main__":
     patterns= content[1].split()
     d = int(content[2])
     #print patterns, d
-    MultipleApproxPatternMatch(text, patterns, d)
+    #MultipleApproxPatternMatch(text, patterns, d)
+    Multiprocessor(text, patterns, d, 5)
